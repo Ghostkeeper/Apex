ac3e3cd4b6b752517a2fadb529baeaa17a47581a
Ghostkeeper <rubend@tutanota.com>
2019-12-17 00:27:34 +0100

    Add test for Vector2D construction
    This is 99% just a test to see if it compiles, as always.

---
+++ test/detail/vector_2d.cpp
@@ -0,0 +1,19 @@
+/*
+ * Library for performing massively parallel computations on polygons.
+ * Copyright (C) 2019 Ghostkeeper
+ * This library is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
+ * You should have received a copy of the GNU Affero General Public License along with this library. If not, see <https://gnu.org/licenses/>.
+ */
+
+#include <gtest/gtest.h> //To run the tests.
+#include "apex/detail/vector_2d.hpp" //The code under test.
+
+namespace apex {
+
+TEST(Vector2D, Empty) {
+	Vector2D<int> vector;
+	EXPECT_EQ(vector.size(), 0) << "The vector is constructed empty.";
+}
+
+}
