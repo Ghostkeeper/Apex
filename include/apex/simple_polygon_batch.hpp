88500e920ac581bbff62a42eb87ba2db922653e8
Ghostkeeper <rubend@tutanota.com>
2020-02-04 23:01:40 +0100

    Add default value to resize function
    The vector spec says it should be an additional function, but I don't see a reason why. This should behave the same and it prevents repetition. Maybe they wrote it that way for explicitness.

--- include/apex/simple_polygon_batch.hpp
+++ include/apex/simple_polygon_batch.hpp
@@ -904,15 +904,18 @@ protected:
 		 *
 		 * If the new size is smaller, vertices are removed.
 		 *
-		 * If the new size is bigger, vertices at position 0,0 will be added.
+		 * If the new size is bigger, the default value is added until it has
+		 * the desired size.
 		 * \param new_size The new size for a container.
+		 * \param default_value A vertex to insert if the simple polygon needs
+		 * to become bigger.
 		 */
-		void resize(const size_t new_size) {
+		void resize(const size_t new_size, const Point2& default_value = Point2(0, 0)) {
 			if(new_size > size()) {
 				reserve(new_size);
 				const size_t start = start_index();
 				for(size_t i = size(); i < new_size; ++i) {
-					batch.vertex_buffer[start + i] = Point2(0, 0);
+					batch.vertex_buffer[start + i] = default_value;
 				}
 			}
 			batch.index_buffer[2 + polygon_index * 3 + 1] = new_size; //Set the new size. This may drop some vertices.
