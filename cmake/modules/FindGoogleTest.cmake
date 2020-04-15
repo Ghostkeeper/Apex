4d8eabb10662dd3e2177c2e8ddf70c016db3a373
Ghostkeeper <rubend@tutanota.com>
2020-04-15 03:13:56 +0200

    Only test with probing when using system library of GoogleTest
    When you're building from source you can easily choose the version number manually so we can be assured it's correct. Also, we wouldn't be able to probe because in the configure stage the library isn't built yet.

--- cmake/modules/FindGoogleTest.cmake
+++ cmake/modules/FindGoogleTest.cmake
@@ -54,6 +54,104 @@ if(USE_SYSTEM_GOOGLETEST)
 		set(GOOGLETEST_BOTH_LIBRARIES "${GTEST_BOTH_LIBRARIES}")
 		add_custom_target(GoogleTest) #Dummy target so that you can ensure GoogleTest gets built as dependency iff necessary.
 	endif()
+
+	#Try to find the version number of GoogleTest by component testing.
+	set(GOOGLETEST_VERSION_MAJOR 1) #Always 1.
+	set(GOOGLETEST_VERSION_MINOR 0) #Start off under the assumption that it's at LEAST version 1.0, the oldest published version.
+
+	find_package(Threads REQUIRED) #Threading library is required for GoogleTest if it's compiled with default parameters, so sadly it's also required to detect version of GoogleThread.
+
+	#Test for v1.1.
+	file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.1.cpp" "#include<gtest/gtest.h>\nTEST(Probe, ExceptionAssertion) { ASSERT_NO_THROW(1 == 0); }")
+	try_compile(_googletest_is_1_1 "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe" "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.1.cpp" LINK_LIBRARIES "${GOOGLETEST_BOTH_LIBRARIES};${CMAKE_THREAD_LIBS_INIT}")
+	if(_googletest_is_1_1)
+		set(GOOGLETEST_VERSION_MINOR 1)
+	endif()
+	unset(_googletest_is_1_1)
+
+	#Test for v1.2.
+	file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.2.cpp" "#include<gtest/gtest.h>\nTEST(Probe, FatalFailureAssertion) { ASSERT_NO_FATAL_FAILURE(1 == 0); }")
+	try_compile(_googletest_is_1_2 "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe" "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.2.cpp" LINK_LIBRARIES "${GOOGLETEST_BOTH_LIBRARIES};${CMAKE_THREAD_LIBS_INIT}")
+	if(_googletest_is_1_2)
+		set(GOOGLETEST_VERSION_MINOR 2)
+	endif()
+	unset(_googletest_is_1_2)
+
+	#Test for v1.3.
+	file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.3.cpp" "#include<gtest/gtest.h>\nTEST(Probe, DeathTest) { ASSERT_EXIT(1 == 0, ::testing::ExitedWithCode(0), \"\"); }")
+	try_compile(_googletest_is_1_3 "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe" "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.3.cpp" LINK_LIBRARIES "${GOOGLETEST_BOTH_LIBRARIES};${CMAKE_THREAD_LIBS_INIT}")
+	if(_googletest_is_1_3)
+		set(GOOGLETEST_VERSION_MINOR 3)
+	endif()
+	unset(_googletest_is_1_3)
+
+	#Test for v1.4 or v1.5.
+	#Can't detect the difference between those since there were no new features in 1.5 that would make compilation break when using 1.4.
+	#Only if threads are not available, but we require threads anyway, so we can't test this.
+	file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.5.cpp" "#include<gtest/gtest.h>\nTEST(Probe, DeathIfSupported) { ASSERT_DEATH_IF_SUPPORTED(1 == 0, \"\"); }")
+	try_compile(_googletest_is_1_5 "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe" "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.5.cpp" LINK_LIBRARIES "${GOOGLETEST_BOTH_LIBRARIES};${CMAKE_THREAD_LIBS_INIT}")
+	if(_googletest_is_1_5)
+		set(GOOGLETEST_VERSION_MINOR 5)
+	endif()
+	unset(_googletest_is_1_5)
+
+	#Test for v1.6 or v1.7.
+	#Can't detect the difference between those since there were no new features in 1.7 that would make compilation break when using 1.6.
+	#There were new features but only in the output of the program in the test log; since we only compile and don't run we can't test this.
+	file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.7.cpp" "#include<gtest/gtest.h>\nTEST(Probe, AddFailureAt) { ADD_FAILURE_AT(\"googletest_probe_v1.7.cpp\", 1); }")
+	try_compile(_googletest_is_1_7 "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe" "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.7.cpp" LINK_LIBRARIES "${GOOGLETEST_BOTH_LIBRARIES};${CMAKE_THREAD_LIBS_INIT}")
+	if(_googletest_is_1_7)
+		set(GOOGLETEST_VERSION_MINOR 7)
+	endif()
+	unset(_googletest_is_1_7)
+
+	#Test for v1.8.
+	file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.8.cpp" "#include<gtest/gtest.h>\nTEST(Probe, Injection) { ASSERT_EQ(1, GTEST_HAS_STD_SHARED_PTR_); }")
+	try_compile(_googletest_is_1_8 "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe" "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.8.cpp" LINK_LIBRARIES "${GOOGLETEST_BOTH_LIBRARIES};${CMAKE_THREAD_LIBS_INIT}")
+	if(_googletest_is_1_8)
+		set(GOOGLETEST_VERSION_MINOR 8)
+	endif()
+	unset(_googletest_is_1_8)
+
+	#Test for v1.10.
+	file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.10.cpp" "#include<gtest/gtest.h>\nclass TestSuite : public ::testing::TestWithParam<int> {};\nTEST_P(TestSuite, ParameterisedTest) {}\nINSTANTIATE_TEST_SUITE_P(ParameterisedInst, TestSuite, ::testing::Values());")
+	try_compile(_googletest_is_1_10 "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe" "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.10.cpp" LINK_LIBRARIES "${GOOGLETEST_BOTH_LIBRARIES};${CMAKE_THREAD_LIBS_INIT}")
+	if(_googletest_is_1_10)
+		set(GOOGLETEST_VERSION_MINOR 10)
+	endif()
+	unset(_googletest_is_1_10)
+
+	set(GOOGLETEST_VERSION_STRING "${GOOGLETEST_VERSION_MAJOR}.${GOOGLETEST_VERSION_MINOR}")
+	if(NOT GoogleTest_FIND_QUIETLY)
+		message(STATUS "Google Test version is: ${GOOGLETEST_VERSION_STRING}.")
+	endif()
+
+	#Check if we have the correct version then as per requirements.
+	if(GoogleTest_FIND_VERSION_EXACT)
+		if((NOT GoogleTest_FIND_VERSION_MAJOR EQUAL GOOGLETEST_VERSION_MAJOR) OR (NOT GoogleTest_FIND_VERSION_MINOR EQUAL GOOGLETEST_VERSION_MINOR))
+			if(GoogleTest_FIND_REQUIRED)
+				message(FATAL_ERROR "Google Test version needs to be exactly ${GoogleTest_FIND_VERSION_MAJOR}.${GoogleTest_FIND_VERSION_MINOR}, but found version ${GOOGLETEST_VERSION_MAJOR}.${GOOGLETEST_VERSION_MINOR}.")
+			else()
+				message(WARNING "Google Test version needs to be exactly ${GoogleTest_FIND_VERSION_MAJOR}.${GoogleTest_FIND_VERSION_MINOR}, but found version ${GOOGLETEST_VERSION_MAJOR}.${GOOGLETEST_VERSION_MINOR}.")
+			endif()
+		endif()
+	else() #Inexact version matching.
+		if(GoogleTest_FIND_VERSION_MAJOR GREATER GOOGLETEST_VERSION_MAJOR)
+			if(GoogleTest_FIND_REQUIRED)
+				message(FATAL_ERROR "Google Test version needs to be at least ${GoogleTest_FIND_VERSION_MAJOR}.${GoogleTest_FIND_VERSION_MINOR}, but found version ${GOOGLETEST_VERSION_MAJOR}.${GOOGLETEST_VERSION_MINOR}.")
+			else()
+				message(WARNING "Google Test version needs to be at least ${GoogleTest_FIND_VERSION_MAJOR}.${GoogleTest_FIND_VERSION_MINOR}, but found version ${GOOGLETEST_VERSION_MAJOR}.${GOOGLETEST_VERSION_MINOR}.")
+			endif()
+		elseif(GoogleTest_FIND_VERSION_MAJOR EQUAL GOOGLETEST_VERSION_MAJOR)
+			if(GoogleTest_FIND_VERSION_MINOR GREATER GOOGLETEST_VERSION_MINOR)
+				if(GoogleTest_FIND_REQUIRED)
+					message(FATAL_ERROR "Google Test version needs to be at least ${GoogleTest_FIND_VERSION_MAJOR}.${GoogleTest_FIND_VERSION_MINOR}, but found version ${GOOGLETEST_VERSION_MAJOR}.${GOOGLETEST_VERSION_MINOR}.")
+				else()
+					message(WARNING "Google Test version needs to be at least ${GoogleTest_FIND_VERSION_MAJOR}.${GoogleTest_FIND_VERSION_MINOR}, but found version ${GOOGLETEST_VERSION_MAJOR}.${GOOGLETEST_VERSION_MINOR}.")
+				endif()
+			endif()
+		endif()
+	endif()
 endif()
 
 if(BUILD_GOOGLETEST)
@@ -82,103 +180,7 @@ if(NOT GOOGLETEST_FOUND)
 	endif()
 endif()
 
-#Try to find the version number of GoogleTest by component testing.
-set(GOOGLETEST_VERSION_MAJOR 1) #Always 1.
-set(GOOGLETEST_VERSION_MINOR 0) #Start off under the assumption that it's at LEAST version 1.0, the oldest published version.
 
-find_package(Threads REQUIRED) #Threading library is required for GoogleTest if it's compiled with default parameters, so sadly it's also required to detect version of GoogleThread.
-
-#Test for v1.1.
-file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.1.cpp" "#include<gtest/gtest.h>\nTEST(Probe, ExceptionAssertion) { ASSERT_NO_THROW(1 == 0); }")
-try_compile(_googletest_is_1_1 "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe" "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.1.cpp" LINK_LIBRARIES "${GOOGLETEST_BOTH_LIBRARIES};${CMAKE_THREAD_LIBS_INIT}")
-if(_googletest_is_1_1)
-	set(GOOGLETEST_VERSION_MINOR 1)
-endif()
-unset(_googletest_is_1_1)
-
-#Test for v1.2.
-file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.2.cpp" "#include<gtest/gtest.h>\nTEST(Probe, FatalFailureAssertion) { ASSERT_NO_FATAL_FAILURE(1 == 0); }")
-try_compile(_googletest_is_1_2 "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe" "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.2.cpp" LINK_LIBRARIES "${GOOGLETEST_BOTH_LIBRARIES};${CMAKE_THREAD_LIBS_INIT}")
-if(_googletest_is_1_2)
-	set(GOOGLETEST_VERSION_MINOR 2)
-endif()
-unset(_googletest_is_1_2)
-
-#Test for v1.3.
-file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.3.cpp" "#include<gtest/gtest.h>\nTEST(Probe, DeathTest) { ASSERT_EXIT(1 == 0, ::testing::ExitedWithCode(0), \"\"); }")
-try_compile(_googletest_is_1_3 "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe" "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.3.cpp" LINK_LIBRARIES "${GOOGLETEST_BOTH_LIBRARIES};${CMAKE_THREAD_LIBS_INIT}")
-if(_googletest_is_1_3)
-	set(GOOGLETEST_VERSION_MINOR 3)
-endif()
-unset(_googletest_is_1_3)
-
-#Test for v1.4 or v1.5.
-#Can't detect the difference between those since there were no new features in 1.5 that would make compilation break when using 1.4.
-#Only if threads are not available, but we require threads anyway, so we can't test this.
-file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.5.cpp" "#include<gtest/gtest.h>\nTEST(Probe, DeathIfSupported) { ASSERT_DEATH_IF_SUPPORTED(1 == 0, \"\"); }")
-try_compile(_googletest_is_1_5 "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe" "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.5.cpp" LINK_LIBRARIES "${GOOGLETEST_BOTH_LIBRARIES};${CMAKE_THREAD_LIBS_INIT}")
-if(_googletest_is_1_5)
-	set(GOOGLETEST_VERSION_MINOR 5)
-endif()
-unset(_googletest_is_1_5)
-
-#Test for v1.6 or v1.7.
-#Can't detect the difference between those since there were no new features in 1.7 that would make compilation break when using 1.6.
-#There were new features but only in the output of the program in the test log; since we only compile and don't run we can't test this.
-file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.7.cpp" "#include<gtest/gtest.h>\nTEST(Probe, AddFailureAt) { ADD_FAILURE_AT(\"googletest_probe_v1.7.cpp\", 1); }")
-try_compile(_googletest_is_1_7 "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe" "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.7.cpp" LINK_LIBRARIES "${GOOGLETEST_BOTH_LIBRARIES};${CMAKE_THREAD_LIBS_INIT}")
-if(_googletest_is_1_7)
-	set(GOOGLETEST_VERSION_MINOR 7)
-endif()
-unset(_googletest_is_1_7)
-
-#Test for v1.8.
-file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.8.cpp" "#include<gtest/gtest.h>\nTEST(Probe, Injection) { ASSERT_EQ(1, GTEST_HAS_STD_SHARED_PTR_); }")
-try_compile(_googletest_is_1_8 "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe" "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.8.cpp" LINK_LIBRARIES "${GOOGLETEST_BOTH_LIBRARIES};${CMAKE_THREAD_LIBS_INIT}")
-if(_googletest_is_1_8)
-	set(GOOGLETEST_VERSION_MINOR 8)
-endif()
-unset(_googletest_is_1_8)
-
-#Test for v1.10.
-file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.10.cpp" "#include<gtest/gtest.h>\nclass TestSuite : public ::testing::TestWithParam<int> {};\nTEST_P(TestSuite, ParameterisedTest) {}\nINSTANTIATE_TEST_SUITE_P(ParameterisedInst, TestSuite, ::testing::Values());")
-try_compile(_googletest_is_1_10 "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe" "${CMAKE_CURRENT_BINARY_DIR}/googletest_probe/googletest_probe_v1.10.cpp" LINK_LIBRARIES "${GOOGLETEST_BOTH_LIBRARIES};${CMAKE_THREAD_LIBS_INIT}")
-if(_googletest_is_1_10)
-	set(GOOGLETEST_VERSION_MINOR 10)
-endif()
-unset(_googletest_is_1_10)
-
-set(GOOGLETEST_VERSION_STRING "${GOOGLETEST_VERSION_MAJOR}.${GOOGLETEST_VERSION_MINOR}")
-if(NOT GoogleTest_FIND_QUIETLY)
-	message(STATUS "Google Test version is: ${GOOGLETEST_VERSION_STRING}.")
-endif()
-
-#Check if we have the correct version then as per requirements.
-if(GoogleTest_FIND_VERSION_EXACT)
-	if((NOT GoogleTest_FIND_VERSION_MAJOR EQUAL GOOGLETEST_VERSION_MAJOR) OR (NOT GoogleTest_FIND_VERSION_MINOR EQUAL GOOGLETEST_VERSION_MINOR))
-		if(GoogleTest_FIND_REQUIRED)
-			message(FATAL_ERROR "Google Test version needs to be exactly ${GoogleTest_FIND_VERSION_MAJOR}.${GoogleTest_FIND_VERSION_MINOR}, but found version ${GOOGLETEST_VERSION_MAJOR}.${GOOGLETEST_VERSION_MINOR}.")
-		else()
-			message(WARNING "Google Test version needs to be exactly ${GoogleTest_FIND_VERSION_MAJOR}.${GoogleTest_FIND_VERSION_MINOR}, but found version ${GOOGLETEST_VERSION_MAJOR}.${GOOGLETEST_VERSION_MINOR}.")
-		endif()
-	endif()
-else() #Inexact version matching.
-	if(GoogleTest_FIND_VERSION_MAJOR GREATER GOOGLETEST_VERSION_MAJOR)
-		if(GoogleTest_FIND_REQUIRED)
-			message(FATAL_ERROR "Google Test version needs to be at least ${GoogleTest_FIND_VERSION_MAJOR}.${GoogleTest_FIND_VERSION_MINOR}, but found version ${GOOGLETEST_VERSION_MAJOR}.${GOOGLETEST_VERSION_MINOR}.")
-		else()
-			message(WARNING "Google Test version needs to be at least ${GoogleTest_FIND_VERSION_MAJOR}.${GoogleTest_FIND_VERSION_MINOR}, but found version ${GOOGLETEST_VERSION_MAJOR}.${GOOGLETEST_VERSION_MINOR}.")
-		endif()
-	elseif(GoogleTest_FIND_VERSION_MAJOR EQUAL GOOGLETEST_VERSION_MAJOR)
-		if(GoogleTest_FIND_VERSION_MINOR GREATER GOOGLETEST_VERSION_MINOR)
-			if(GoogleTest_FIND_REQUIRED)
-				message(FATAL_ERROR "Google Test version needs to be at least ${GoogleTest_FIND_VERSION_MAJOR}.${GoogleTest_FIND_VERSION_MINOR}, but found version ${GOOGLETEST_VERSION_MAJOR}.${GOOGLETEST_VERSION_MINOR}.")
-			else()
-				message(WARNING "Google Test version needs to be at least ${GoogleTest_FIND_VERSION_MAJOR}.${GoogleTest_FIND_VERSION_MINOR}, but found version ${GOOGLETEST_VERSION_MAJOR}.${GOOGLETEST_VERSION_MINOR}.")
-			endif()
-		endif()
-	endif()
-endif()
 
 mark_as_advanced(GOOGLETEST_INCLUDE_DIRS)
 mark_as_advanced(GOOGLETEST_LIBRARIES)
