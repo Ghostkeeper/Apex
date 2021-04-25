ad8f4ce1638c7f164b42acc7181c29dff1ac467a
Ghostkeeper <rubend@tutanota.com>
2021-04-25 23:14:06 +0200

    Don't name area function header specific to simple_polygon
    We're going to need a bunch of very similar implementations of computing the area. One will be to compute the area of a simple_polygon (aggregate all vertices of a vector to a single area). One will be to compute the area of a complex polygon (aggregate all vertices of multiple vectors to a single area). One will be to compute the area of a batch of simple polygons (aggregate all vertices of a set of vectors to a matching set of areas). And one will be to compute the area of a batch of complex polygons (aggregate all vertices of multiple vectors of vectors to a matching set of areas). Each of these will be slightly different, but likely share the overall behaviour and theory. For this reason, I'd like to put all of them together even though they will be functions of various different classes.

--- test/helpers/mock_simple_polygon.hpp
+++ test/helpers/mock_simple_polygon.hpp
@@ -1,6 +1,6 @@
 /*
  * Library for performing massively parallel computations on polygons.
- * Copyright (C) 2020 Ghostkeeper
+ * Copyright (C) 2021 Ghostkeeper
  * This library is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
  * You should have received a copy of the GNU Affero General Public License along with this library. If not, see <https://gnu.org/licenses/>.
@@ -12,8 +12,8 @@
 #include <vector> //Provides the base mock implementation.
 
 #include "apex/point2.hpp" //The coordinates of the polygon.
-#include "apex/detail/simple_polygon_area.hpp" //To implement the CRTP to allow these classes to be cast to the mock.
-#include "apex/detail/simple_polygon_translate.hpp"
+#include "apex/detail/area.hpp" //To implement the CRTP to allow these classes to be cast to the mock.
+#include "apex/detail/translate.hpp"
 
 namespace apex {
 
