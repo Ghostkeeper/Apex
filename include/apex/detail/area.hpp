1c2c82dafe5909564d29ccf51e8d60473e452415
Ghostkeeper <rubend@tutanota.com>
2021-04-26 01:10:50 +0200

    Add multi-threaded implementation of calculating polygon area
    This is a try-out of using OpenMP. I've used it before so OpenMP itself is not a problem, but we should get the build system to work properly here which is more of a problem.

--- include/apex/detail/area.hpp
+++ include/apex/detail/area.hpp
@@ -9,6 +9,8 @@
 #ifndef APEX_AREA_HPP
 #define APEX_AREA_HPP
 
+#include <omp.h> //To do parallel processing.
+
 #include "../coordinate.hpp" //To return area_t.
 
 namespace apex {
@@ -78,8 +80,12 @@ protected:
 	 * rectangle formed by \f$x_2 \cdot y_1\f$ minus the area formed by the
 	 * other rectangle formed by \f$x_1 \cdot y_2\f$. In other words, the area
 	 * of the parallelogram is \f$x_2 \cdot y_1 - x_1 \cdot y_2\f$. This needs
-	 * to be divided by two to arrive at the area of the triangle. This is the
-	 * shoelace formula.
+	 * to be divided by two to arrive at the area of the triangle. The surface
+	 * area of a simple polygon is the sum of all of these triangles. This is
+	 * the shoelace formula.
+	 *
+	 * In this implementation, the areas of these parallelograms are calculated
+	 * in sequence and summed together to get the area of the simple polygon.
 	 * \return The surface area of the simple polygon.
 	 */
 	area_t area_st() const {
@@ -89,6 +95,63 @@ protected:
 			previous = vertex;
 		}
 		return area / 2; //Instead of dividing each triangle's area by 2, simply divide the total by 2 afterwards.
+	}
+
+	/*!
+	 * Multi-threaded implementation of ``area``.
+	 *
+	 * This uses the shoelace formula to compute the area. The shoelace formula
+	 * sums the areas of the individual triangles formed by two adjacent
+	 * vertices and the coordinate origin.
+	 *
+	 * To calculate the area of a triangle with one vertex on the origin, we'll
+	 * calculate the area of a parallelogram formed by the original triangle and
+	 * that triangle mirrored around the line segment we're calculating the area
+	 * for. Visualise this:
+	 *
+	 * @image html res/shoelace_algorithm_parallelogram.svg
+	 *
+	 * The area of the parallelogram can be visualised by starting with a
+	 * rectangle that encloses the original triangle like this. The green areas
+	 * fall outside of the parallelogram and shouldn't be counted towards its
+	 * area.
+	 *
+	 * @image html res/shoelace_algorithm_rectangle_overlay.svg
+	 *
+	 * The green areas are not part of the parallelogram here, but they can be
+	 * shifted towards the missing part that falls outside of the rectangle like
+	 * this.
+	 *
+	 * @image html res/shoelace_algorithm_multiple_rectangles.svg
+	 *
+	 * This forms a second rectangle, in this case a smaller one in the upper
+	 * right hand corner. The two green triangles partially overlap and go
+	 * partially outside of the parallelogram we're trying to get the area of.
+	 * The part that is overlap plus the part that goes outside of the
+	 * parallelogram together forms an area of \f$x_1 \cdot y_2\f$.
+	 *
+	 * The total area of the parallelogram then becomes the area of the
+	 * rectangle formed by \f$x_2 \cdot y_1\f$ minus the area formed by the
+	 * other rectangle formed by \f$x_1 \cdot y_2\f$. In other words, the area
+	 * of the parallelogram is \f$x_2 \cdot y_1 - x_1 \cdot y_2\f$. This needs
+	 * to be divided by two to arrive at the area of the triangle. The surface
+	 * area of a simple polygon is the sum of all of these triangles. This is
+	 * the shoelace formula.
+	 *
+	 * In this implementation, the areas of these parallelograms are calculated
+	 * in parallel. This is summed with a parallel reduction, to produce the
+	 * area of the complete polygon.
+	 * \return The surface area of the simple polygon.
+	 */
+	area_t area_mt() const {
+		area_t area = 0;
+		const size_t size = base().size();
+		#pragma omp parallel for reduction(+:area)
+		for(size_t vertex = 0; vertex < size; ++vertex) {
+			size_t previous = (vertex - 1 + size) % size;
+			area += static_cast<area_t>(base()[previous].x) * base()[vertex].y - static_cast<area_t>(base()[previous].y) * base()[vertex].x;
+		}
+		return area / 2;
 	}
 
 private:
