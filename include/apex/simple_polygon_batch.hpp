412d484e8648b0e26d0de1f6f85a82b831372d83
Ghostkeeper <rubend@tutanota.com>
2021-05-08 00:15:43 +0200

    Add implementation of getting the areas of a batch of simple polygons
    For single-threaded, nothing performs better than just asking every simple polygon in sequence what its area is.

--- include/apex/simple_polygon_batch.hpp
+++ include/apex/simple_polygon_batch.hpp
@@ -1,6 +1,6 @@
 /*
  * Library for performing massively parallel computations on polygons.
- * Copyright (C) 2020 Ghostkeeper
+ * Copyright (C) 2021 Ghostkeeper
  * This library is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
  * You should have received a copy of the GNU Affero General Public License along with this library. If not, see <https://gnu.org/licenses/>.
@@ -56,7 +56,10 @@ namespace apex {
  * \tparam SimplePolygon An implementation of simple polygons to use. Used to
  * swap out dependencies in automated tests.
  */
-class SimplePolygonBatch {
+class SimplePolygonBatch :
+		//Implementing the private functions in separate classes with Curiously Recurring Template Pattern.
+		public SimplePolygonBatchArea<SimplePolygonBatch> {
+
 protected:
 	class Reference;
 
