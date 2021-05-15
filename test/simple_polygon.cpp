e9a1c784480d18d46dedaff89ec0ba95e7b18bc3
Ghostkeeper <rubend@tutanota.com>
2021-05-15 21:29:22 +0200

    std::sqrt is not constexpr
    Officially it is not. GCC makes it constexpr but this fails on Clang.

--- test/simple_polygon.cpp
+++ test/simple_polygon.cpp
@@ -1,6 +1,6 @@
 /*
  * Library for performing massively parallel computations on polygons.
- * Copyright (C) 2020 Ghostkeeper
+ * Copyright (C) 2021 Ghostkeeper
  * This library is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
  * You should have received a copy of the GNU Affero General Public License along with this library. If not, see <https://gnu.org/licenses/>.
@@ -39,7 +39,7 @@ public:
 		triangle.emplace_back(60, 60);
 
 		constexpr coord_t width = 1000;
-		constexpr coord_t corner_size = width * (std::sqrt(2) - 1);
+		const coord_t corner_size = width * (std::sqrt(2) - 1);
 		octagon.emplace_back(corner_size, 0);
 		octagon.emplace_back(width - corner_size, 0);
 		octagon.emplace_back(width, corner_size);
