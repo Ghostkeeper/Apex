0feabcea16a792d4af56a8225ffab24d67330f06
Ghostkeeper <rubend@tutanota.com>
2021-05-20 00:24:44 +0200

    Also ignore conversions in Clang
    Apparently the pop of this diagnostic is also applied to Clang, even though it's written for GCC. Popping it individually for Clang gives an error that there's nothing to pop.

--- test/coordinate.cpp
+++ test/coordinate.cpp
@@ -1,6 +1,6 @@
 /*
  * Library for performing massively parallel computations on polygons.
- * Copyright (C) 2019 Ghostkeeper
+ * Copyright (C) 2021 Ghostkeeper
  * This library is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
  * You should have received a copy of the GNU Affero General Public License along with this library. If not, see <https://gnu.org/licenses/>.
@@ -31,6 +31,8 @@ TEST(Coordinate, CoordTRange) {
 	EXPECT_EQ(x, -0x80000000) << "Coordinates must be 32-bits, so integers must overflow here.";
 
 #pragma GCC diagnostic ignored "-Woverflow"
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wconstant-conversion"
 	x = 0x8000000000000000;
 #pragma GCC diagnostic pop
 	EXPECT_EQ(x, 0) << "Coordinates must be 32-bits, so only the lower bits must have been interpreted here.";
@@ -45,6 +47,8 @@ TEST(Coordinate, CoordTRange) {
 	EXPECT_EQ(x, 0x7FFFFFFF) << "Coordinates must be 32-bits, so integers must underflow here.";
 
 #pragma GCC diagnostic ignored "-Woverflow"
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wconstant-conversion"
 	x = -0x8000000000000001;
 #pragma GCC diagnostic pop
 	EXPECT_EQ(x, -1) << "Coordinates must be 32-bits, so only the lower bits must have been interpreted here.";
