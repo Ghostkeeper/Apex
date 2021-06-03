97d2c753aa3b2350e3ccee04b8647b782d31598b
Ghostkeeper <rubend@tutanota.com>
2021-06-04 00:02:08 +0200

    Add constructor implementations for the batch base
    We already have these constructors in the current Batch implementation, but they'll need to stay there because they will be specialised with different types.

--- include/apex/detail/batch_base.hpp
+++ include/apex/detail/batch_base.hpp
@@ -9,6 +9,8 @@
 #ifndef APEX_BATCH_BASE
 #define APEX_BATCH_BASE
 
+#include <vector> //Providing the base data structure to store elements in.
+
 namespace apex {
 
 /*!
@@ -25,13 +27,66 @@ namespace apex {
  */
 template<typename Element>
 class BatchBase {
-	protected:
-		/*!
-		 * The main data contained in the batch.
-		 *
-		 * The batch behaves almost exactly like this vector.
-		 */
-		std::vector<Element> elements;
+public:
+	/*!
+	 * Constructs an empty batch.
+	 */
+	BatchBase() noexcept {}
+
+	/*!
+	 * Construct a batch containing the specified element repeated a number of
+	 * times.
+	 * \param count The amount of elements to fill the batch with.
+	 * \param value The element to fill the batch with. This element gets
+	 * repeated a number of times.
+	 */
+	BatchBase(const size_t count, const Element& value = Element()) : elements(count, value) {}
+
+	/*!
+	 * Construct a batch containing default-inserted instances of the element.
+	 * \param count The amount of elements to fill the batch with.
+	 */
+	BatchBase(const size_t count) : elements(count) {}
+
+	/*!
+	 * Construct a batch with the contents of the range ``[first, last)``.
+	 * \param first The iterator to start copying from.
+	 * \param last An iterator signalling that the range of elements to copy has
+	 * ended.
+	 */
+	template<class InputIterator>
+	BatchBase(InputIterator first, InputIterator last) : elements(first, last) {}
+
+	/*!
+	 * Construct a copy of the specified batch.
+	 *
+	 * All elements inside the batch will be copied as well.
+	 * \param other The batch to copy.
+	 */
+	BatchBase(const BatchBase& other) : elements(other.elements) {}
+
+	/*!
+	 * Move constructor, moving one batch to another location using move
+	 * semantics.
+	 *
+	 * After the move, ``other`` is guaranteed to be empty.
+	 * \param other The batch to move into the new batch.
+	 */
+	BatchBase(BatchBase&& other) noexcept : elements(std::move(other.elements)) {}
+
+	/*!
+	 * Constructs a batch from the contents of an initialiser list.
+	 * \param initialiser_list The list of elements to put in the new batch.
+	 */
+	BatchBase(std::initializer_list<Element> initialiser_list) : elements(initialiser_list) {}
+
+protected:
+	/*!
+	 * The main data contained in the batch.
+	 *
+	 * The batch behaves almost exactly like this vector.
+	 */
+	std::vector<Element> elements;
 };
 
 }
