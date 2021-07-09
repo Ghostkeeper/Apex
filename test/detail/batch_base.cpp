69bf8533d886a5698bb35e33833c25caa6bebe8e
Ghostkeeper <rubend@tutanota.com>
2021-07-09 22:01:29 +0200

    Add test for constructing a batch of copies of a subbatch

--- test/detail/batch_base.cpp
+++ test/detail/batch_base.cpp
@@ -42,11 +42,17 @@ public:
 	BatchBase<int> one_two;
 
 	/*!
+	 * A batch with numbers 1, 2, 3, 4, 5, 6, 7, 8 and 9.
+	 */
+	BatchBase<int> one_through_nine;
+
+	/*!
 	 * Constructs the fixture batches.
 	 */
 	void SetUp() {
 		one.assign({1});
 		one_two.assign({1, 2});
+		one_through_nine.assign({1, 2, 3, 4, 5, 6, 7, 8, 9});
 	}
 };
 
@@ -66,16 +72,46 @@ TEST(BatchOfBatches, ConstructEmpty) {
  * subbatches.
  */
 TEST(BatchOfBatches, ConstructDefault) {
-	BatchBase<BatchBase<int>> batch1(1);
+	const BatchBase<BatchBase<int>> batch1(1);
 	EXPECT_EQ(batch1.size(), 1) << "We added one empty subbatch.";
 	EXPECT_TRUE(batch1[0].empty()) << "The subbatch was created with its default constructor, which should leave it empty.";
 
-	BatchBase<BatchBase<int>> batch4(4);
+	const BatchBase<BatchBase<int>> batch4(4);
 	EXPECT_EQ(batch4.size(), 4) << "We added 4 default-constructed subbatches.";
 	EXPECT_TRUE(batch4[0].empty()) << "The subbatch was created with its default constructor, which should leave it empty.";
 	EXPECT_TRUE(batch4[1].empty()) << "The subbatch was created with its default constructor, which should leave it empty.";
 	EXPECT_TRUE(batch4[2].empty()) << "The subbatch was created with its default constructor, which should leave it empty.";
 	EXPECT_TRUE(batch4[3].empty()) << "The subbatch was created with its default constructor, which should leave it empty.";
+}
+
+/*!
+ * Tests constructing a batch of batches by making copies of one subbatch.
+ */
+TEST_F(BatchOfBatchesFixture, ConstructCopies) {
+	const BatchBase<BatchBase<int>> batch1_1(1, one);
+	EXPECT_EQ(batch1_1.size(), 1) << "There is one subbatch in this batch.";
+	EXPECT_EQ(batch1_1[0], one) << "The subbatch is the one with one element.";
+
+	const BatchBase<BatchBase<int>> batch4_1(4, one);
+	EXPECT_EQ(batch4_1.size(), 4) << "There are four copies of the subbatch in this batch.";
+	EXPECT_EQ(batch4_1[0], one) << "The subbatches are copies of the original.";
+	EXPECT_EQ(batch4_1[1], one) << "The subbatches are copies of the original.";
+	EXPECT_EQ(batch4_1[2], one) << "The subbatches are copies of the original.";
+	EXPECT_EQ(batch4_1[3], one) << "The subbatches are copies of the original.";
+
+	const BatchBase<BatchBase<int>> batch4_12(4, one_two);
+	EXPECT_EQ(batch4_12.size(), 4) << "There are four copies of the subbatch in this batch.";
+	EXPECT_EQ(batch4_12[0], one_two) << "The subbatches are copies of the original.";
+	EXPECT_EQ(batch4_12[1], one_two) << "The subbatches are copies of the original.";
+	EXPECT_EQ(batch4_12[2], one_two) << "The subbatches are copies of the original.";
+	EXPECT_EQ(batch4_12[3], one_two) << "The subbatches are copies of the original.";
+
+	const BatchBase<BatchBase<int>> batch4_19(4, one_through_nine);
+	EXPECT_EQ(batch4_19.size(), 4) << "There are four copies of the subbatch in this batch.";
+	EXPECT_EQ(batch4_19[0], one_through_nine) << "The subbatches are copies of the original.";
+	EXPECT_EQ(batch4_19[1], one_through_nine) << "The subbatches are copies of the original.";
+	EXPECT_EQ(batch4_19[2], one_through_nine) << "The subbatches are copies of the original.";
+	EXPECT_EQ(batch4_19[3], one_through_nine) << "The subbatches are copies of the original.";
 }
 
 }
