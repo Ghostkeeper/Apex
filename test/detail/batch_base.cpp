58daff49a7cdd8c8bc491ff235617651e62fdacd
Ghostkeeper <rubend@tutanota.com>
2021-09-18 22:22:23 +0200

    Add test for inserting via move

--- test/detail/batch_base.cpp
+++ test/detail/batch_base.cpp
@@ -1092,7 +1092,7 @@ TEST_F(BatchOfBatchesFixture, EmplaceBackInitialiserList) {
 }
 
 /*!
- * Tests inserting a subbatch into a batch by making a copy.
+ * Test inserting a subbatch into a batch by making a copy.
  */
 TEST_F(BatchOfBatchesFixture, InsertCopy) {
 	BatchBase<BatchBase<int>> batch = linear_increases; //Make a copy so that we can compare with the original.
@@ -1106,6 +1106,28 @@ TEST_F(BatchOfBatchesFixture, InsertCopy) {
 	EXPECT_EQ(batch, BatchBase<BatchBase<int>>({one_through_nine, linear_increases[0], linear_increases[1], linear_increases[2], linear_increases[3], linear_increases[4], one})) << "We added the one batch at the end.";
 
 	batch.insert(batch.begin() + 2, one_two);
+	EXPECT_EQ(batch.size(), linear_increases.size() + 3) << "We added a third subbatch.";
+	EXPECT_EQ(batch, BatchBase<BatchBase<int>>({one_through_nine, linear_increases[0], one_two, linear_increases[1], linear_increases[2], linear_increases[3], linear_increases[4], one})) << "We added the one_two batch in the middle.";
+}
+
+/*!
+ * Test inserting a subbatch into a batch by moving it in.
+ */
+TEST_F(BatchOfBatchesFixture, InsertMove) {
+	BatchBase<BatchBase<int>> batch = linear_increases; //Make a copy of each input, so that we can compare with the original.
+	BatchBase<int> one_through_nine_copy = one_through_nine;
+	BatchBase<int> one_copy = one;
+	BatchBase<int> one_two_copy = one_two;
+
+	batch.insert(batch.begin(), std::move(one_through_nine_copy));
+	EXPECT_EQ(batch.size(), linear_increases.size() + 1) << "We added a new subbatch, so the size grew by one.";
+	EXPECT_EQ(batch, BatchBase<BatchBase<int>>({one_through_nine, linear_increases[0], linear_increases[1], linear_increases[2], linear_increases[3], linear_increases[4]})) << "We added the one_through_nine batch at the front.";
+
+	batch.insert(batch.end(), std::move(one_copy));
+	EXPECT_EQ(batch.size(), linear_increases.size() + 2) << "We added another subbatch, so the size grew again.";
+	EXPECT_EQ(batch, BatchBase<BatchBase<int>>({one_through_nine, linear_increases[0], linear_increases[1], linear_increases[2], linear_increases[3], linear_increases[4], one})) << "We added the one batch at the end.";
+
+	batch.insert(batch.begin() + 2, std::move(one_two_copy));
 	EXPECT_EQ(batch.size(), linear_increases.size() + 3) << "We added a third subbatch.";
 	EXPECT_EQ(batch, BatchBase<BatchBase<int>>({one_through_nine, linear_increases[0], one_two, linear_increases[1], linear_increases[2], linear_increases[3], linear_increases[4], one})) << "We added the one_two batch in the middle.";
 }
