bff9fecb1f0470678af4b5a3c5873a26eac6e614
Ghostkeeper <rubend@tutanota.com>
2021-11-29 23:23:08 +0100

    Add GPU implementation of area for batches of polygons
    Not testable yet, since the MockSimplePolygonBatch doesn't have a subelement_data or size_subelements function yet.

--- include/apex/area.hpp
+++ include/apex/area.hpp
@@ -23,13 +23,22 @@ namespace detail {
 //Declare the detail functions so that we can reference them from the public ones.
 template<polygonal SimplePolygon>
 area_t area_st(const SimplePolygon& polygon);
+
 template<multi_polygonal SimplePolygonBatch>
-std::vector<area_t> area_st(const SimplePolygonBatch& polygon);
+std::vector<area_t> area_st(const SimplePolygonBatch&);
+
 template<polygonal SimplePolygon>
 area_t area_mt(const SimplePolygon& polygon);
+
+template<multi_polygonal SimplePolygonBatch>
+std::vector<area_t> area_mt(const SimplePolygonBatch&);
+
 #ifdef GPU
 template<polygonal SimplePolygon>
 area_t area_gpu(const SimplePolygon& polygon);
+
+template<multi_polygonal SimplePolygonBatch>
+std::vector<area_t> area_gpu(const SimplePolygonBatch&);
 #endif //GPU
 
 };
@@ -334,6 +343,7 @@ std::vector<area_t> area_mt(const SimplePolygonBatch& batch) {
  * In this implementation, the areas of these parallelograms are calculated on
  * the GPU, if available. This is summed with a parallel reduction, to produce
  * the area of the complete polygon.
+ * \tparam SimplePolygon A class that behaves like a simple polygon.
  * \param polygon The simple polygon to calculate the area of.
  * \return The surface area of the simple polygon.
  */
@@ -348,6 +358,79 @@ area_t area_gpu(const SimplePolygon& polygon) {
 		area += static_cast<area_t>(vertices[previous].x) * vertices[vertex].y - static_cast<area_t>(vertices[previous].y) * vertices[vertex].x;
 	}
 	return area / 2;
+}
+
+/*!
+ * Implementation of ``area`` that runs on the graphics card, if available.
+ *
+ * This uses the shoelace formula to compute the area. The shoelace formula sums
+ * the areas of the individual triangles formed by two adjacent vertices and the
+ * coordinate origin.
+ *
+ * To calculate the area of a triangle with one vertex on the origin, we'll
+ * calculate the area of a parallelogram formed by the original triangle and
+ * that triangle mirrored around the line segment we're calculating the area
+ * for. Visualise this:
+ *
+ * @image html res/shoelace_algorithm_parallelogram.svg
+ *
+ * The area of the parallelogram can be visualised by starting with a rectangle
+ * that encloses the original triangle like this. The green areas fall outside
+ * of the parallelogram and shouldn't be counted towards its area.
+ *
+ * @image html res/shoelace_algorithm_rectangle_overlay.svg
+ *
+ * The green areas are not part of the parallelogram here, but they can be
+ * shifted towards the missing part that falls outside of the rectangle like
+ * this.
+ *
+ * @image html res/shoelace_algorithm_multiple_rectangles.svg
+ *
+ * This forms a second rectangle, in this case a smaller one in the upper right
+ * hand corner. The two green triangles partially overlap and go partially
+ * outside of the parallelogram we're trying to get the area of. The part that
+ * is overlap plus the part that goes outside of the parallelogram together
+ * forms an area of \f$x_1 \cdot y_2\f$.
+ *
+ * The total area of the parallelogram then becomes the area of the rectangle
+ * formed by \f$x_2 \cdot y_1\f$ minus the area formed by the other rectangle
+ * formed by \f$x_1 \cdot y_2\f$. In other words, the area of the parallelogram
+ * is \f$x_2 \cdot y_1 - x_1 \cdot y_2\f$. This needs to be divided by two to
+ * arrive at the area of the triangle. The surface area of a simple polygon is
+ * the sum of all of these triangles. This is the shoelace formula.
+ *
+ * In this implementation, the areas of these parallelograms are calculated on
+ * the GPU, if available. This is summed with a parallel reduction, to produce
+ * the area of the complete polygon. This is repeated in parallel for each
+ * polygon in the batch.
+ * \tparam SimplePolygonBatch A class that behaves like a batch of simple
+ * polygons.
+ * \param batch The batch of simple polygons to calculate the areas of.
+ * \return A list of areas, one for each simple polygon, in the same order as
+ * the order of those polygons in the batch.
+ */
+template<multi_polygonal SimplePolygonBatch>
+std::vector<area_t> area_gpu(const SimplePolygonBatch& batch) {
+	std::vector<area_t> result;
+	result.resize(batch.size()); //Resize, so that all threads can enter their data in parallel.
+
+	const Point2* vertices = batch.subelement_data();
+	const size_t vertices_size = batch.size_subelements();
+	#pragma omp target teams distribute parallel for map(to:vertices) map(from:result)
+	for(size_t polygon_index = 0; polygon_index < batch.size(); ++polygon_index) {
+		area_t area = 0;
+		const auto& polygon = batch[polygon_index];
+		const size_t size = polygon.size();
+
+		//On the GPU we'll spawn new threads for each sub-loop too.
+		#pragma omp parallel for reduction(+:area)
+		for(size_t vertex = 0; vertex < size; ++vertex) {
+			size_t previous = (vertex - 1 + size) % size;
+			area += static_cast<area_t>(polygon[previous].x) * polygon[vertex].y - static_cast<area_t>(polygon[previous].y) * polygon[vertex].x;
+		}
+		result[polygon_index] = area / 2;
+	}
+	return result;
 }
 #endif //GPU
 
