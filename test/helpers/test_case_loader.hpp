7e185dc2cbbe1cbe0c67974290bd0bbec0f1ac21
Ghostkeeper <rubend@tutanota.com>
2021-12-01 20:47:36 +0100

    Add definition for helper function to load cases from SVG
    This is development by documentation: First write the specs in the documentation, then implement those.

---
+++ test/helpers/test_case_loader.hpp
@@ -0,0 +1,38 @@
+/*
+ * Library for performing massively parallel computations on polygons.
+ * Copyright (C) 2021 Ghostkeeper
+ * This library is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
+ * You should have received a copy of the GNU Affero General Public License along with this library. If not, see <https://gnu.org/licenses/>.
+ */
+
+#ifndef APEX_TEST_CASE_LOADER
+#define APEX_TEST_CASE_LOADER
+
+#include <string>
+
+#include "apex/simple_polygon.hpp" //We can construct simple polygons here.
+
+namespace apex {
+
+/*!
+ * Load a test case from a file as a simple polygon.
+ *
+ * This will search through the ``simple_polygon`` folder in the test cases for
+ * an SVG file with the given name, followed by .svg. In that SVG file it will
+ * look for a ``<polygon>`` tag and parse its coordinates. Those coordinates are
+ * used to construct a simple polygon. This way, the easy-to-visualise SVG files
+ * can be used to create test cases.
+ *
+ * Only the first polygon in the file is used. No transformations or CSS is
+ * applied. Other SVG elements such as ``<path>`` are not parsed. The parser in
+ * this loader is extremely simple and naive. This prevents needing a whole new
+ * library just to read test cases.
+ * \param name The name of the test case to load.
+ * \return A SimplePolygon with the loaded test data.
+ */
+SimplePolygon load_simple_polygon(const std::string& name);
+
+}
+
+#endif //APEX_TEST_CASE_LOADER
