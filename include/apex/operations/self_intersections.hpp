3c8cdaae504c262e066e46370370ef703a36634f
Ghostkeeper <rubend@tutanota.com>
2022-05-21 17:10:23 +0200

    Add multi-threaded implementation of self-intersections
    Almost done, but not quite.

--- include/apex/operations/self_intersections.hpp
+++ include/apex/operations/self_intersections.hpp
@@ -146,7 +146,7 @@ Batch<PolygonSelfIntersection> self_intersections_st_naive(const Polygon& polygo
 				}
 				const Point2 other_a = polygon[other_index];
 				const Point2 other_b = polygon[other_index + 1]; //No need to limit to polygon size, since this can never equal segment_index.
-				std::optional<Point2> intersection = LineSegment::intersect(this_a, this_b, other_a, other_b);
+				const std::optional<Point2> intersection = LineSegment::intersect(this_a, this_b, other_a, other_b);
 				if(intersection) { //They did intersect.
 					if((position_index[segment_index] == position_index[other_index + 1] && *intersection == this_a) || (position_index[(segment_index + 1) % polygon.size()] == position_index[other_index] && *intersection == this_b)) { //But it's intersecting at the endpoints with only 0-length segments in between.
 						continue; //Don't count those. They are essentially just along the same contour.
@@ -162,6 +162,120 @@ Batch<PolygonSelfIntersection> self_intersections_st_naive(const Polygon& polygo
 					result.emplace_back(this_a, previous_index, segment_index);
 				}
 			}
+		}
+	}
+	return result;
+}
+
+template<polygonal Polygon>
+Batch<PolygonSelfIntersection> self_intersections_mt_naive(const Polygon& polygon) {
+	Batch<PolygonSelfIntersection> result;
+	if(polygon.size() == 2) [[unlikely]] {
+		/* In the case of 2 vertices, we have a degenerate polygon with 2 line
+		segments that loop back on each other, completely overlapping. The
+		algorithm below would find two intersections because each line checks
+		for overlap with the previous line (with clock arithmetic). But there is
+		only one. So we can special-case that. */
+		result.emplace_back(polygon[0], 0, 1); //The 0th segment always intersects with the 1st segment. Choose any point on the line as intersection point.
+	} else if(polygon.size() > 2) [[likely]] {
+		//Pre-compute the unique positions along the contour, to find and ignore zero-length edges.
+		//We divide the polygon into segments, one for each thread. These segments are then sewn together afterwards.
+		std::vector<size_t> position_index;
+		position_index.resize(polygon.size());
+		const size_t thread_count = omp_get_num_threads();
+		const size_t vertices_per_thread = polygon.size() / thread_count;
+		const size_t leftover = polygon.size() % thread_count; //The first few threads get 1 extra, if it's not divisible by the thread count.
+		#pragma omp parallel for
+		for(size_t thread = 0; thread < thread_count; ++thread) {
+			const size_t start_vertex = vertices_per_thread * thread + std::min(thread, leftover);
+			const size_t end_vertex = start_vertex + vertices_per_thread + (thread < leftover);
+			Point2 last_position = polygon[start_vertex];
+			size_t unique_position = start_vertex; //Likely too high if there are any duplicates in previous segments, but for the algorithm this doesn't matter.
+			for(size_t vertex = start_vertex + 1; vertex < end_vertex; ++vertex) {
+				if(polygon[vertex] != last_position) {
+					unique_positon++;
+					last_position = polygon[vertex];
+				}
+				position_index[vertex] = unique_position;
+			}
+		}
+		//If the vertices around the segment seams are equal, sow the seams to indicate that the positions are equal.
+		for(size_t thread = 0; thread < thread_count; ++thread) {
+			const size_t start_vertex = vertices_per_thread * thread + std::min(thread, leftover);
+			const size_t end_vertex = start_vertex + vertices_per_thread + (thread < leftover);
+			const size_t previous_vertex = (start_vertex + polygon.size() - 1) % polygon.size();
+			if(polygon[previous_vertex] == polygon[start_vertex]) {
+				const size_t old_position_index = position_index[start_vertex];
+				for(size_t vertex = start_vertex; vertex < end_vertex && position_index[vertex] == old_position_index; ++vertex) {
+					postion_index[vertex] = position_index[previous_index];
+				}
+			}
+		}
+
+		/* To parallellise this 2D triangular loop, we create a single index
+		enumerating all pairs of edges, and parallelise over those indices. From
+		that index we can calculate the segment indices A and B back.
+		What we want is a triangular loop such as this (for 7 vertices):
+		-   -   -   -   -   -   -
+		-   -   -   -   -   -   -
+		0x2 -   -   -   -   -   -
+		0x3 1x3 -   -   -   -   -
+		0x4 1x4 2x4 -   -   -   -
+		0x5 1x5 2x5 3x5 -   -   -
+		0x6 1x6 2x6 3x6 4x6 -   -
+		To achieve such a loop, we can fold the bottom half of this loop onto
+		the right side of the top half. To do that, we'll mirror the bottom half
+		in both directions. First in the A dimension:
+		-   -   -   -   -   -   -
+		-   -   -   -   -   -   -
+		0x2 -   -   -   -   -   -
+		0x3 1x3 -   -   -   -   -
+		0x4 1x4 2x4 -   -   -   -
+		-   -   3x5 2x5 1x5 0x5 -
+		-   4x6 3x6 2x6 1x6 0x6 -
+		And then in the B dimension too:
+		-   -   -   -   -   -   -
+		-   -   -   -   -   -   -
+		0x2 4x6 3x6 2x6 1x6 0x6 -
+		0x3 1x3 3x5 2x5 1x5 0x5 -
+		0x4 1x4 2x4 -   -   -   -
+		-   -   -   -   -   -   -
+		-   -   -   -   -   -   -
+		By then iterating the correct amount of times ((N-1)*(N-2)/2, in this
+		case 15) and selecting the correct rectangle, we can simply pretend this
+		is a rectangular 2D loop and correct the coordinates for the part that
+		needs to be mirrored. */
+		const size_t num_pairs = (polygon.size() - 1) * (polygon.size() - 2) / 2;
+		#pragma omp parallel for
+		for(size_t pair_index = 0; pair_index < num_pairs; ++pair_index) {
+			size_t segment_a = pair_index % (polygon.size() - 1);
+			size_t segment_b = pair_index / (polygon.size() - 1);
+			if(segment_a > segment_b) {
+				segment_a = polygon.size() - 1 - segment_a; //Mirror A dimension.
+				segment_b = polygon.size() - 2 - segment_b; //Mirror B dimension.
+			}
+			segment_b += 2;
+
+			if(segment_a == 0 && segment_b == polygon.size() - 1) {
+				continue; //Don't check the last vs. the first segment, as they are also neighbours.
+			}
+			if(position_index[segment_index] == position_index[other_index]) { //Same position, so this is a zero-length segment.
+				continue; //Skip. They may not intersect anything (and the segment intersection check doesn't deal with this well).
+			}
+			const Point2 a_start = polygon[segment_a];
+			const Point2 a_end = polygon[segment_a + 1]; //Since B > A, we don't need to check if this exceeds the polygon size.
+			const Point2 b_start = polygon[segment_b];
+			const Point2 b_end = polygon[(segment_b + 1) % polygon.size()];
+			const std::optional<Point2> intersection = LineSegment::intersect(a_start, a_end, b_start, b_end);
+			if(intersection) { //They did intersect.
+				//TODO: Report intersection. We may have to make thread-local arrays that are later combined?
+			}
+		}
+
+		//We skipped each vertex' neighbour. Check now for self-intersection with the neighbour. This can only partially overlap, never properly intersect.
+		#pragma omp parallel for
+		for(size_t vertex = 0; vertex < polygon.size(); ++vertex) { //Check the two adjacent edges around this vertex.
+			//TODO.
 		}
 	}
 	return result;
