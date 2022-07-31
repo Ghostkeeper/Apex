da9c70f3f5c9a6e178cf7a001b154be5f69717ac
Ghostkeeper <rubend@tutanota.com>
2022-07-31 13:43:06 +0200

    Add documentation of enumerate_pairs
    It's quite extensive to explain how it all works. But I want to remember how it works years down the line.

--- include/apex/detail/pairing_function.hpp
+++ include/apex/detail/pairing_function.hpp
@@ -46,6 +46,71 @@ size_t num_pairings(const size_t num_elements, const bool include_adjacent = tru
 	return (num_elements - 1) * (num_elements - !include_adjacent * 2) / 2;
 }
 
+/*!
+ * Get a pair of indices to elements in some list.
+ *
+ * Increasing the index monotonically will eventually and efficiently enumerate
+ * all pairs in a list with a certain given size. Optionally, this can also skip
+ * some pairs that are adjacent in the list (where the difference between the
+ * indices is 1).
+ *
+ * This function is an inverse pairing function. It unpacks a single integer
+ * into a pair of integers, effectively enumerating those pairs. The pairing
+ * function used here is a custom one optimised for enumerating a finite set of
+ * pairs in a triangle. To visualise the order in which pairs are enumerated,
+ * consider the following grid of possible pairs.
+ *
+ * <table>
+ * <caption id="multi_row">Pairs of indices</caption>
+ * <tr><td>0 x 1</td></tr>
+ * <tr><td>0 x 2</td> <td>1 x 2</td></tr>
+ * <tr><td>0 x 3</td> <td>1 x 3</td> <td>2 x 3</td></tr>
+ * <tr><td>0 x 4</td> <td>1 x 4</td> <td>2 x 4</td> <td>3 x 4</td></tr>
+ * <tr><td>0 x 5</td> <td>1 x 5</td> <td>2 x 5</td> <td>3 x 5</td> <td>4 x 5</td></tr>
+ * <tr><td>0 x 6</td> <td>1 x 6</td> <td>2 x 6</td> <td>3 x 6</td> <td>4 x 6</td> <td>5 x 6</td> <td></td></tr>
+ * </table>
+ *
+ * Enumerating this table is complex, but to simplify that we could transform it
+ * into a grid by folding the table in on itself. First we'll mirror the bottom
+ * half of the table.
+ *
+ * <table>
+ * <caption id="multi_row">Mirrored bottom half</caption>
+ * <tr><td>0 x 1</td></tr>
+ * <tr><td>0 x 2</td> <td>1 x 2</td></tr>
+ * <tr><td>0 x 3</td> <td>1 x 3</td> <td>2 x 3</td></tr>
+ * <tr><td>     </td> <td>     </td> <td>     </td> <td>3 x 4</td> <td>2 x 4</td> <td>1 x 4</td> <td>0 x 4</td></tr>
+ * <tr><td>     </td> <td>     </td> <td>4 x 5</td> <td>3 x 5</td> <td>2 x 5</td> <td>1 x 5</td> <td>0 x 5</td></tr>
+ * <tr><td>     </td> <td>5 x 6</td> <td>4 x 6</td> <td>3 x 6</td> <td>2 x 6</td> <td>1 x 6</td> <td>0 x 6</td></tr>
+ * </table>
+ *
+ * Then we can flip the bottom half of the table to fold it into the top half.
+ * The two halves don't have any overlap then.
+ *
+ * <table>
+ * <caption id="multi_row">Folded bottom half</caption>
+ * <tr><td>0 x 1</td> <td>5 x 6</td> <td>4 x 6</td> <td>3 x 6</td> <td>2 x 6</td> <td>1 x 6</td> <td>0 x 6</td></tr>
+ * <tr><td>0 x 2</td> <td>1 x 2</td> <td>4 x 5</td> <td>3 x 5</td> <td>2 x 5</td> <td>1 x 5</td> <td>0 x 5</td></tr>
+ * <tr><td>0 x 3</td> <td>1 x 3</td> <td>2 x 3</td> <td>3 x 4</td> <td>2 x 4</td> <td>1 x 4</td> <td>0 x 4</td></tr>
+ * </table>
+ *
+ * This table efficiently enumerates all possible pairs in a set of 7 elements,
+ * has a known size and can be traversed simply with a modulus.
+ *
+ * Enumerating beyond the number of pairs in the set produces indeterminate
+ * results. The indices will no longer be within the range of the set.
+ * \param num_elements The number of elements in a set to enumerate the pairs
+ * of.
+ * \param index The index of the pair to return. Incrementing this between 0 and
+ * the result of \ref num_pairings will eventually enumerate all pairs of
+ * indices.
+ * \param include_adjacent Whether to count adjacent elements as well.
+ * \return A pair of indices in the set of elements.
+ */
+std::pair<size_t, size_t> enumerate_pairs(const size_t num_elements, const size_t index, const bool include_adjacent = true) {
+	//TODO.
+}
+
 }
 
 }
