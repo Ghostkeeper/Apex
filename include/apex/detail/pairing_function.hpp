9f5cdb0684aaa145aafde7d2219c339fd3f95e93
Ghostkeeper <rubend@tutanota.com>
2022-08-01 15:40:31 +0200

    Invert include_adjacent usage
    This was a logic error on my part. It's adding one when adjacent elements are NOT allowed.

--- include/apex/detail/pairing_function.hpp
+++ include/apex/detail/pairing_function.hpp
@@ -119,10 +119,10 @@ std::pair<size_t, size_t> enumerate_pairs(const size_t num_elements, const size_
 		a = index % (num_elements - 1);
 		b = index / (num_elements - 1);
 		if(a > b) {
-			a = num_elements - 1 - include_adjacent - a; //Mirror A dimension. -1 for iterating up to size-1, another -1 if we skip adjacent vertices here.
+			a = num_elements - 1 - !include_adjacent - a; //Mirror A dimension. -1 for iterating up to size-1, another -1 if we skip adjacent vertices here.
 			b = num_elements - 1 - b; //Mirror B dimension. -1 for iterating up to size-1
 		} else {
-			b += 1 + include_adjacent; //Don't compare the same vertex or adjacent vertices.
+			b += 1 + !include_adjacent; //Don't compare the same vertex or adjacent vertices.
 		}
 	}
 	return {a, b};
